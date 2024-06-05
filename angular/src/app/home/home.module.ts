354317deb81ec1c9445e3c246d51fa98c735dfb7
Namdinh16 <namdinh2k03@gmail.com>
2024-06-05 16:11:01 +0700

    hien thi doanh thu theo tung nam

--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -3,8 +3,9 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ChartModule } from 'primeng/chart';
+import { DropdownModule } from 'primeng/dropdown';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, HomeRoutingModule, ChartModule],
+  imports: [SharedModule, HomeRoutingModule, ChartModule, DropdownModule],
 })
 export class HomeModule {}
