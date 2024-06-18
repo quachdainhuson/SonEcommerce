863096d9074463a4024568e2079806184aff4ccd
NhuSonNe <nhuson35a@gmail.com>
2024-06-18 23:11:42 +0700

    Gửi Email khi tạo hóa đơn

---
+++ aspnet-core/src/SonEcommerce.Domain/Emailing/EmailSettingProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Settings;
+
+namespace SonEcommerce.Emailing
+{
+    public class EmailSettingProvider : SettingDefinitionProvider
+    {
+        private readonly ISettingEncryptionService encryptionService;
+
+        public EmailSettingProvider(ISettingEncryptionService encryptionService)
+        {
+            this.encryptionService = encryptionService;
+        }
+
+        public override void Define(ISettingDefinitionContext context)
+        {
+            var passSetting = context.GetOrNull("Abp.Mailing.Smtp.Password");
+            if (passSetting != null)
+            {
+                string debug = encryptionService.Encrypt(passSetting, "e6624683fffc5230715c21b75160e997-6fafb9bf-f3daf42f");
+            }
+
+        }
+    }
+}
---
+++ aspnet-core/src/SonEcommerce.Public.Application.Contracts/Orders/OrderEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SonEcommerce.Public.Orders
+{
+    public class OrderEmailViewModel
+    {
+        public OrderDto Order { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
